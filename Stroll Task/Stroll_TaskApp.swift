698a76e86bcdb770488f44da9f7b698b3a5156a6
cikoedofebrians <ciko.edo.febrians@gmail.com>
2025-06-20 20:57:00 +0700

    Initial Commit

---
+++ Stroll Task/Stroll_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stroll_TaskApp.swift
+//  Stroll Task
+//
+//  Created by Ciko Edo Febrian on 20/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Stroll_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
